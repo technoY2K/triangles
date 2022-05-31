be7c2b7da5467cc6dcc16d13b75d6268960f4468
technoY2K <cloud@kevia.me>
2022-05-31 15:49:18 -0700

    chore: just use an iframe

---
+++ app/routes/resume.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+        <div className="container mx-auto">
+            <iframe className="w-full aspect-video" src="/resume.pdf" />
+        </div>
+    );
+}
