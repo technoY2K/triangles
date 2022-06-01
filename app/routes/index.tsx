3881dc1fd0244e2b2de401a98be52a2fd278d6b9
technoY2K <cloud@kevia.me>
2022-05-31 18:00:45 -0700

    feat: fetching block info and displaying it

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,12 +4,25 @@ import { useLoaderData } from "@remix-run/react";
 import { type MeshProps, Canvas, useFrame } from "@react-three/fiber";
 import provider from "~/web3/provider";
 import { type Block } from "@ethersproject/abstract-provider";
+import { utils } from "ethers";
+
+type LoaderData = {
+    block: Block;
+    gas: string;
+};
 
 export const loader: LoaderFunction = async () => {
     const latestBlockNumber = await provider.getBlockNumber();
     const block = await provider.getBlock(latestBlockNumber);
+    const rawGasPrice = await provider.getGasPrice();
+    const gas = utils.formatUnits(rawGasPrice, "gwei");
 
-    return json(block);
+    const data: LoaderData = {
+        block,
+        gas,
+    };
+
+    return json(data);
 };
 
 type ShapeProps = MeshProps & {
@@ -34,15 +47,28 @@ const Box = (props: ShapeProps) => {
 };
 
 export default function Index() {
-    const block = useLoaderData<Block>();
+    const data = useLoaderData<LoaderData>();
 
     return (
-        <section className="h-[91vh]">
-            <div className="bg-[#44475a] h-5/6">
+        <section className="h-[91vh] pt-8">
+            <div className="mb-4">
+                {data ? (
+                    <div className="text-white text-lg">
+                        <p className="text-2xl text-[#f1fa8c] mb-4">
+                            Ethereum Block Stats
+                        </p>
+                        <p>{`Block Number: ${data.block.number}`}</p>
+                        <p>{`Gas Price (Gwei): ${data.gas}`}</p>
+                    </div>
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </div>
+            <div className="bg-[#44475a] h-2/3">
                 <Canvas>
                     <ambientLight />
                     <pointLight position={[10, 10, 10]} />
-                    <Box position={[0, 0.2, 2]} meshColor="red" />
+                    <Box position={[0, 0.2, 2]} meshColor="#bd93f9" />
                 </Canvas>
             </div>
         </section>
