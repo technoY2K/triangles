68894739fa5debdf547946bcbc928326bde3b1c0
technoY2K <cloud@kevia.me>
2022-06-01 20:56:22 -0700

    refactor: trying a mobile detector

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,5 +1,14 @@
+import { isMobile } from "react-device-detect";
+
 export default function About() {
-    return (
+    return isMobile ? (
+        <div>
+            <p className="text-center text-[#f1fa8c] text-3xl mt-16">
+                For the best experience, please view on a desktop browser. (Rèsumè is in
+                an iframe 🙇🏽‍♂️)
+            </p>
+        </div>
+    ) : (
         <div className="h-[91vh]">
             <iframe
                 src="/resume.pdf"
