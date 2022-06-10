632ff27d097afba1364ab02cbacc7cd2601f1e71
technoY2K <cloud@kevia.me>
2022-06-09 20:38:56 -0700

    refactor: wrap ethers provider around a type alias

--- app/web3/provider.tsx
+++ app/web3/provider.tsx
@@ -1,5 +1,23 @@
 import { ethers } from "ethers";
+import { type Block } from "@ethersproject/abstract-provider";
+import { utils } from "ethers";
+
+type Provider<B> = {
+    getBlockNumber: () => Promise<number>;
+    getBlock: (n: number) => Promise<B>;
+    getGasPrice: () => Promise<string>;
+};
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.INFURA);
 
-export default provider;
+const p: Provider<Block> = {
+    getBlockNumber: provider.getBlockNumber,
+    getBlock: (n: number) => provider.getBlock(n),
+    getGasPrice: async () => {
+        const rawGasPrice = await provider.getGasPrice();
+        const gas = utils.formatUnits(rawGasPrice, "gwei");
+        return gas;
+    },
+};
+
+export default p;
